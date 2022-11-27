2a5832357289e6db46fca2aa866bbe587c256692
afzalahmad1 <afzalm117@gmail.com>
2022-11-27 21:04:39 +0530

    dummy commit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -12,23 +12,33 @@ public class Pizza {
     }
 
     public int getPrice(){
+        if(isVeg){
+            this.price=300;
+        }
+        else{
+            this.price=400;
+        }
         return this.price;
     }
 
     public void addExtraCheese(){
         // your code goes here
+        this.price+=80;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        this.price+=70;
     }
 
     public void addTakeaway(){
         // your code goes here
+        this.price+=20;
     }
 
     public String getBill(){
         // your code goes here
+        this.bill+=this.price;
         return this.bill;
     }
 }
