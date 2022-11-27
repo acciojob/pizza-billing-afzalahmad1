a2530e863b6c7212da3ae79533cd4103ff116ebb
afzalahmad1 <afzalm117@gmail.com>
2022-11-28 00:25:33 +0530

    Succes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,10 +3,10 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
     DeluxePizza dp = new DeluxePizza(true);
-    System.out.println(dp.getPrice());
+    //System.out.println(dp.getPrice());
     dp.addTakeaway();
     dp.addExtraCheese();
-    dp.addTakeaway();
+    //dp.addTakeaway();
     System.out.println(dp.getBill());
 
     Pizza p = new Pizza(true);
@@ -16,3 +16,10 @@ public class Main {
     System.out.println(p.getBill());
   }
 }
+
+ /* Veg pizza base price = 300
+Non-veg pizza base price = 400
+        Extra Cheese Price = 80
+        Extra Toppings For Veg Pizza = 70
+        Extra Toppings For Non-veg Pizza = 120
+        Paper bag Price = 20*/
